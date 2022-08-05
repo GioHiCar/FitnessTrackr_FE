cceaccaeae5fd3f17a9d8239aa42a803e13c5e18
giohicar <giohicar@gmail.com>
2022-08-05 16:11:41 -0500

    added deleteRoutines back to myRoutines

--- src/components/MyRoutines.js
+++ src/components/MyRoutines.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate,Link } from "react-router-dom";
-import { ValidUser, getUserRoutines, addRoutine} from "../api";
+import { ValidUser, getUserRoutines, addRoutine, deleteRoutines} from "../api";
 
 
 const MyRoutines = () => {
